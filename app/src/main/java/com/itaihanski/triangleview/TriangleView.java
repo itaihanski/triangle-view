42a977d238698270b2d7125020ab92e6fbb320df
Lorenzo Stanco <lorenzo.stanco@gmail.com>
2016-04-19 08:20:49 +0200

    Fixed indentation

--- app/src/main/java/com/itaihanski/triangleview/TriangleView.java
+++ app/src/main/java/com/itaihanski/triangleview/TriangleView.java
@@ -92,7 +92,7 @@ public class TriangleView extends View {
         mPaint = new Paint();
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setColor(mColor);
-		mPaint.setAntiAlias(true);
+        mPaint.setAntiAlias(true);
     }
 
     //
